ec04dd9df520978033062760f2f379b00b949408
abberadhi <abbe.radhi11@gmail.com>
2020-10-11 23:57:37 +0200

    able to add people to the meeting & recieve post request in the backend

--- server/src/controllers/meetings.js
+++ server/src/controllers/meetings.js
@@ -52,6 +52,53 @@ router.get('/',
   }
 );
 
+
+router.post('/create',
+  async function(req, res) {
+    // if (!req.isAuthenticated()) {
+      console.log(req.body);
+    if (false) {
+      // Redirect unauthenticated requests to home page
+      res.locals.message = 'Access forbidden: You have to log in first!';
+      res.redirect('/')
+    } else {
+      let params = {
+        active: { meetings: true }
+      };
+
+      // Get the access token
+      var accessToken;
+      try {
+        accessToken = await tokens.getAccessToken(req);
+      } catch (err) {
+        req.flash('error_msg', {
+          message: 'Could not get access token. Try signing out and signing in again.',
+          debug: JSON.stringify(err)
+        });
+      }
+
+      if (accessToken && accessToken.length > 0) {
+        try {
+          // Get the events
+          var events = await graph.getEvents(accessToken);
+          params.events = events.value;
+          console.log("params.events", params.events);
+        } catch (err) {
+          console.log(err);
+          req.flash('error_msg', {
+            message: 'Could not fetch events',
+            debug: JSON.stringify(err)
+          });
+        }
+      } else {
+        req.flash('error_msg', 'Could not get an access token');
+      }
+
+      res.render('create', params);
+    }
+  }
+);
+
 /* GET /meetings/create */
 router.get('/create',
   async function(req, res) {
